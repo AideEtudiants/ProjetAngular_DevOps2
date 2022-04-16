2cedd39e52b8ab3cb81ff55c4d75194224ec2e29
AKKOURA <64647807+AKKOURA@users.noreply.github.com>
2022-04-17 00:45:41 +0200

    notification v0

--- src/app/components/barre-de-recherche/barre-de-recherche.component.ts
+++ src/app/components/barre-de-recherche/barre-de-recherche.component.ts
@@ -24,6 +24,7 @@ export class BarreDeRechercheComponent  implements OnInit {
   data:any='';
   filteredOptions: Observable<string[]>;
   public totalItem : number ;
+  totalNotif : number ;
   productList : ProductEntity[];
   // currentUser: User;
 
