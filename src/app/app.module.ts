7bf2c427968f8eb9099d1a37a163e3d15832bcdd
LydiaSM <l.smahi@hotmail.fr>
2022-04-29 13:02:56 +0200

    ajout chatbot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,6 +33,7 @@ import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { AnswerComponent } from './components/answer/answer.component';
 import { ConnexionComponent } from './components/connexion/connexion.component';
+import { ChatComponent } from './components/chat/chat.component';
 
 
 const routes: Routes = [
@@ -77,6 +78,7 @@ const routes: Routes = [
     ParticiperForm,
     AnswerComponent,
     ConnexionComponent,
+    ChatComponent,
   ],
   imports: [
     BrowserModule,
